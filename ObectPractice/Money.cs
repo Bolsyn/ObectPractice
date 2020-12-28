e0e20acb5b4ba03ddc25c22aa219d5910cfb0a84
Bolsyn <Serikagis@yandex.ru>
2020-12-28 21:24:09 +0600

    Add project files.

---
+++ ObectPractice/Money.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObectPractice
+{
+    public class Money
+    {
+        private int count;
+        private int cost;
+
+        public int Cost { get; set; }
+        public int Count { get; set; }
+
+        public int SumOfMoney
+        {
+            get
+            {
+                return cost * count;
+            }
+        }
+        public Money(int count, int cost)
+        {
+            this.cost = cost;
+            this.count = count;
+        }
+
+        public void About()
+        {
+            Console.Write("Cost = ");
+            Console.WriteLine(cost);
+            Console.Write("Count = ");
+            Console.WriteLine(count);
+        }
+
+        public bool IsEnoughtToBuy(int needMoney)
+        {
+            if (needMoney > cost)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int CountOfItemsEnoight(int costOfItem)
+        {
+            return cost / costOfItem;
+        }
+
+
+    }
+}
---
+++ ObectPractice/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ObectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
